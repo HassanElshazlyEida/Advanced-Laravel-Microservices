70d4e78cc776994426db85320aecb881675645f2
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-10-06 22:30:40 +0300

    👌 IMPROVE: ( api call ) Container network instead of direct access

--- laravel-ambassador/app/Services/UserService.php
+++ laravel-ambassador/app/Services/UserService.php
@@ -6,6 +6,6 @@ class UserService extends ApiService
 {
     public function __construct()
     {
-        $this->url = 'http://host.docker.internal:8001/api'; # User Service URL
+        $this->url = 'http://users_ms:8000/api'; # User Service URL
     }
 }
