6b1ac1b244f6b0327d8275cfd2fa07c4922bd612
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-10-06 19:08:32 +0300

    👌 IMPROVE: run queue on different topic

--- laravel-ambassador/app/Http/Controllers/OrderController.php
+++ laravel-ambassador/app/Http/Controllers/OrderController.php
@@ -105,11 +105,11 @@ class OrderController extends Controller
         $order->complete = 1;
         $order->save();
 
-        // $array = $order->toArray();
+        $array = $order->toArray();
 
-        // $array['ambassador_revenue'] = $order->ambassador_revenue;
+        $array['ambassador_revenue'] = $order->ambassador_revenue;
 
-        // OrderCompleted::dispatch($array);
+        OrderCompleted::dispatch($array)->onQueue('email_topic');
 
         return [
             'message' => 'success'
