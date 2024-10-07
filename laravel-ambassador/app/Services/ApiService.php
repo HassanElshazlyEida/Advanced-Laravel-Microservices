d58dfa844a77c016e70d7f0fc304696eb4314175
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-10-07 16:07:34 +0300

    🔨 Refactor Code:  Api service

--- laravel-ambassador/app/Services/ApiService.php
+++ laravel-ambassador/app/Services/ApiService.php
@@ -8,14 +8,30 @@ abstract class ApiService
 {
     protected string $url;
 
-    public function post($path,$data) {
-        return \Http::post($this->url.$path,$data);
-    }
-    public function get($path) {
-        return \Http::acceptJson()->withHeaders(
+
+    public function request($method,$path,$data = []) {
+        $response = \Http::acceptJson()->withHeaders(
         [
             'Authorization' => 'Bearer '.request()->cookie('jwt')
         ]
-        )->get($this->url.$path)->json();
+        )->$method($this->url.$path,$data);
+
+        if($response->ok()) {
+            return $response->json();
+        }
+        throw new \Exception($response->body());
+    }
+
+    public function post($path,$data) {
+        return $this->request('post',$path,$data);
+    }
+    public function get($path) {
+        return $this->request('get',$path);
+    }
+    public function put($path,$data) {
+        return $this->request('put',$path,$data);
+    }
+    public function delete($path) {
+        return $this->request('delete',$path);
     }
 }
