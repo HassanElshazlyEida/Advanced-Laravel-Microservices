ef9e646246c633cae9535e68a57eb347bd93e3e1
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-10-07 22:08:50 +0300

    💥 NEW: Link job & product (update + delete)

---
+++ checkout-service/app/Jobs/LinkCreated.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Link;
+use App\Models\LinkProduct;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class LinkCreated implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public array $link
+    )
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Link::create($this->link);
+
+        LinkProduct::insert($this->link['link_products']);
+    }
+}
--- laravel-ambassador/app/Http/Controllers/LinkController.php
+++ laravel-ambassador/app/Http/Controllers/LinkController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Services\UserService;
 use App\Http\Resources\LinkResource;
+use App\Jobs\LinkCreated;
 
 class LinkController extends Controller
 {
@@ -34,13 +35,19 @@ class LinkController extends Controller
             'user_id' => $user->id,
             'code' => Str::random(6)
         ]);
-
+        $linkProducts = [];
         foreach ($request->input('products') as $product_id) {
-            LinkProduct::create([
+            $linkProduct =LinkProduct::create([
                 'link_id' => $link->id,
                 'product_id' => $product_id
             ]);
+            $linkProducts[] = $linkProduct->toArray();
         }
+
+        $array = $link->toArray();
+        $array['link_products'] = $linkProducts;
+
+        LinkCreated::dispatch($array)->onQueue('checkout_topic');
 
         return $link;
     }
