2d5c1ef1f0d21f4613476545f4e7a0569e2dbb67
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-10-13 21:41:10 +0300

    👌 IMPROVE: required files for ambassador service

---
+++ ambassador-service/app/Http/Controllers/LinkController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+use App\Models\LinkProduct;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Services\UserService;
+use App\Http\Resources\LinkResource;
+use App\Jobs\LinkCreated;
+
+class LinkController extends Controller
+{
+    public function __construct(
+        protected UserService $userService
+    )
+    {
+
+        
+    }
+    
+   
+    public function store(Request $request)
+    {
+        $user = $this->userService->get('/user');
+
+        $link = Link::create([
+            'user_id' => $user['id'],
+            'code' => Str::random(6)
+        ]);
+        $linkProducts = [];
+        foreach ($request->input('products') as $product_id) {
+            $linkProduct =LinkProduct::create([
+                'link_id' => $link->id,
+                'product_id' => $product_id
+            ]);
+            $linkProducts[] = $linkProduct->toArray();
+        }
+
+        $array = $link->toArray();
+        $array['link_products'] = $linkProducts;
+        
+        LinkCreated::dispatch($array)->onQueue('checkout_topic');
+
+        return $link;
+    }
+
+
+}
--- ambassador-service/routes/api.php
+++ ambassador-service/routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Ambassador
+Route::prefix('ambassador')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::get('products/frontend', [ProductController::class, 'frontend']);
+    Route::get('products/backend', [ProductController::class, 'backend']);
+    
+    Route::middleware(['scope.ambassador'])->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::put('users/info', [AuthController::class, 'updateInfo']);
+        Route::put('users/password', [AuthController::class, 'updatePassword']);
+
+        Route::post('links', [LinkController::class, 'store']);
+        Route::get('stats', [StatsController::class, 'index']);
+        Route::get('rankings', [StatsController::class, 'rankings']);
+    });
+    
 });
